9a5854d70b81fb7397676517140d745bc40541c1
privrja <privrja2@fit.cvut.cz>
2018-09-13 11:28:48 +0200

    Main view
    | change form

--- application/controllers/Land.php
+++ application/controllers/Land.php
@@ -2,12 +2,31 @@
 
 class Land extends CI_Controller {
 
-    public function index() {
-        $data = array();
+    /**
+     * Land constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper("form");
+    }
 
-        $this->load->view("templates/header");
-        $this->load->view("pages/main", $data);
-        $this->load->view("templates/footer");
+    public function index() {
+        $this->load->library("form_validation");
+
+        $data['title'] = 'Block';
+
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('text', 'Text', 'required');
+
+        if ($this->form_validation->run() === false) {
+            $this->load->view('templates/header');
+            $this->load->view('pages/main');
+            $this->load->view('templates/footer');
+
+        } else {
+            $this->news_model->set_news();
+            $this->load->view('pages/block');
+        }
     }
 
 
--- application/views/pages/main.php
+++ application/views/pages/main.php
@@ -3,62 +3,45 @@
 </div>
 <div id="div-right">
 
-    <fieldset>
-        <label for="search"><span>Search in</span><select name="search" class="select">
-                <option value="pubchem">PubChem</option>
-                <option value="chemspider">ChemSpider</option>
-                <option value="norine">Norine</option>
-                <option value="pdb">PDB</option>
-            </select></label>
-    </fieldset>
+    <?php echo validation_errors(); ?>
 
-    <fieldset>
-        <label for="name"><span>Name</span><input type="text" id="txt-canvas-name" class="txt-def" name="name"/></label>
-        <button onclick="find()">FIND</button>
-        <input type="text" id="txt-canvas-sug-name" class="txt-def none" title="Suggested name"/>
-        <button id="btn-canvas-translate-name" class="none" onclick="translateName()"><i class="fa fa-arrow-left"></i> <i class="fa fa-arrow-left"></i></button>
-    </fieldset>
+    <?php echo form_open('land/index', array('class' => 'form')); ?>
 
-    <fieldset>
-        <label for="smile">
-            <span>SMILE</span>
-            <textarea id="txt-canvas-smile" class="txt-area" name="smile"></textarea>
-        </label>
-        <button onclick="find()">FIND</button>
-    </fieldset>
+    <label for="search">Search in</label>
+    <select name="search" class="select">
+        <option value="pubchem">PubChem</option>
+        <option value="chemspider">ChemSpider</option>
+        <option value="norine">Norine</option>
+        <option value="pdb">PDB</option>
+    </select>
 
-    <fieldset>
-        <label><span>&nbsp;</span></label>
-        <textarea id="txt-canvas-sug-smile" class="txt-area none" title="Suggested SMILE"></textarea>
-        <button id="btn-canvas-translate-smile" class="none" onclick="translateSmile()"><i class="fa fa-arrow-up"></i> <i class="fa fa-arrow-up"></i></button>
-    </fieldset>
+    <label for="name">Name</label>
+    <input type="text" id="txt-canvas-name" class="txt-def" name="name"/>
+    <button class="btn-find" onclick="find()">Find</button>
 
-    <fieldset>
-        <label for="fle"><span>Formula</span><input type="text" id="txt-canvas-fle" class="txt-def" name="fle"/></label>
-        <button onclick="find()">FIND</button>
-        <input type="text" id="txt-canvas-sug-fle" class="txt-def none" title="Suggested formula"/>
-        <button id="btn-canvas-translate-fle" class="none" onclick="translateFle()"><i class="fa fa-arrow-left"></i> <i class="fa fa-arrow-left"></i></button>
-    </fieldset>
+    <label for="smile">SMILE</label>
+    <textarea id="txt-canvas-smile" class="txt-area" name="smile"></textarea>
+    <button class="btn-find" onclick="find()">Find</button>
 
-    <fieldset>
-        <label for="id"><span>ID</span><input type=text class="txt-def" name="id"/></label>
-        <button onclick="find()">FIND</button>
-    </fieldset>
+    <label for="fle">Formula</label>
+    <input type="text" id="txt-canvas-fle" class="txt-def" name="fle"/>
+    <button class="btn-find" onclick="find()">Find</button>
 
-    <fieldset>
-        <label><span>&nbsp;</span></label>
-        <button id="button-canvas-easy-smile" class="btn-same" onclick="easy()">Easy SMILE</button>
-        <button id="btn-canvas-disintegrate" class="btn-same">Disintegrate</button>
-    </fieldset>
+    <label for="id">ID</label>
+    <input type=text class="txt-def" name="id"/>
+    <button class="btn-find" onclick="find()">Find</button>
 
-    <fieldset>
-        <label><span>&nbsp;</span></label>
-        <button id="btn-canvas-translate-all" class="btn-same" onclick="translateAll()"><i class="fa fa-arrow-left"></i> <i class="fa fa-arrow-left"></i> <i class="fa fa-arrow-left"></i></button>
-    </fieldset>
+    <label>&nbsp;</label>
+    <button id="button-canvas-easy-smile" class="btn-same" onclick="easy()">Canonical SMILES</button>
+    <br />
 
-    <fieldset>
-        <label><span>&nbsp;</span></label>
-        <input type="submit" class="btn-same" value="Save to DB"/>
-    </fieldset>
+    <label>&nbsp;</label>
+    <button id="btn-canvas-disintegrate" class="btn-same">Generate Building Blocks</button>
+    <br />
+
+    <label>&nbsp;</label>
+    <input type="submit" class="btn-same" value="Save to DB"/>
+
+    </form>
 
 </div>
