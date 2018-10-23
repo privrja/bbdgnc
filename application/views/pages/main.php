cedb6f4fb7e4742941a6b57af879ee7b5282d913
privrja <privrja2@fit.cvut.cz>
2018-10-23 10:46:06 +0200

    Dev
    | fix exact match repopulating

--- application/views/pages/main.php
+++ application/views/pages/main.php
@@ -23,7 +23,7 @@ use Bbdgnc\Enum\Front;
            value="<?= $name ?>"/>
 
     <label for="chk-match" class="chk">Exact match</label>
-    <input type="checkbox" id="chk-match" name="<?= Front::CANVAS_INPUT_MATCH ?>" value="1" checked="checked"/>
+    <input type="checkbox" id="chk-match" name="<?= Front::CANVAS_INPUT_MATCH ?>" value="1" <?= set_checkbox(Front::CANVAS_INPUT_MATCH, '1',true); ?>/>
 
     <label for="txt-canvas-smile">SMILES</label>
     <textarea id="txt-canvas-smile" class="txt-area" name="<?= Front::CANVAS_INPUT_SMILE ?>"
