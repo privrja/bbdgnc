728f8ad269ff5fb388e213034abef6ccc9c4694e
Jan Přívratský <36856494+privrja@users.noreply.github.com>
2018-11-04 16:10:01 +0100

    Dependencies (#16)
    * Dependencies
    
    + add package.json
    
    * Dependencies
    
    + manage smilesDrawer assets
    | update .gitignore

--- application/helpers/asset_helper.php
+++ application/helpers/asset_helper.php
@@ -6,6 +6,10 @@ class AssetHelper {
         return base_url() . 'assets/';
     }
 
+    public static function nodeModulesUrl() {
+        return base_url() . 'node_modules/';
+    }
+
     public static function cssUrl() {
         return AssetHelper::assetUrl() . "css/";
     }
@@ -16,6 +20,10 @@ class AssetHelper {
 
     public static function imgUrl() {
         return AssetHelper::assetUrl() . "img/";
+    }
+
+    public static function jsSmilesDrawer() {
+        return AssetHelper::nodeModulesUrl() . 'smiles-drawer/dist/smiles-drawer.js';
     }
 
 }
