f4397430bf47856a3c6f38c7f8b7fc06467d8057
privrja <privrja2@fit.cvut.cz>
2019-01-14 14:16:39 +0100

    Blocks #32
    | upgrade to php 7.2
    | set up tests

--- tests/base/ComputeMassTest.php
+++ tests/base/ComputeMassTest.php
@@ -4,8 +4,9 @@ namespace Bbdgnc\Test\Base;
 
 use Bbdgnc\Base\FormulaHelper;
 use Bbdgnc\Exception\IllegalArgumentException;
+use PHPUnit\Framework\TestCase;
 
-final class ComputeMassTest extends \PHPUnit\Framework\TestCase {
+final class ComputeMassTest extends TestCase {
 
     public function testComputeMassWithRightData() {
         $result = FormulaHelper::computeMass('C62H111N11O12');
