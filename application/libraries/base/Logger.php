64f4b45973076c8e3258aeaff6f26e4de892533b
privrja <privrja@gmail.com>
2019-02-13 13:02:59 +0100

    CycloBranch #3
    | update logger

--- application/libraries/base/Logger.php
+++ application/libraries/base/Logger.php
@@ -14,13 +14,11 @@ class Logger {
         if ($lvl === LoggerEnum::DISABLE || $lvl < self::LEVEL) {
             return;
         }
-        $fd = fopen(self::FILE . date("Y-m-d", time()) . ".php", 'a');
         $bt = debug_backtrace();
         $caller = array_shift($bt);
         $str = LoggerEnum::$values[$lvl] . ' - ' . date('Y-m-d h:i:s', time())
             . " --> " . $msg . " in " . $caller['file'] . " on line: " . $caller['line'] . "\n";
-        fwrite($fd, $str);
-        fclose($fd);
+        file_put_contents(self::FILE . date("Y-m-d", time()) . ".php", $str, FILE_APPEND);
     }
 
 }
