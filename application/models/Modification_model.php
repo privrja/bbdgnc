28c83badf3436ddccf5798a1e321b4b2d195b1a8
privrja <privrja2@fit.cvut.cz>
2019-02-19 23:44:50 +0100

    CycloBranch #3
    + prepare Modification model

---
+++ application/models/Modification_model.php
@@ -0,0 +1,22 @@
+<?php
+
+use Bbdgnc\TransportObjects\ModificationTO;
+
+class Modification_model extends CI_Model {
+
+    const TABLE_NAME = 'modification';
+
+    public function __construct() {
+        $this->load->database();
+    }
+
+    public function getAll() {
+        $query = $this->db->get(self::TABLE_NAME);
+        return $query->result_array();
+    }
+
+    public function insertSequence(ModificationTO $modificationTO) {
+        $this->db->insert(self::TABLE_NAME, $modificationTO->asModification());
+    }
+
+}
