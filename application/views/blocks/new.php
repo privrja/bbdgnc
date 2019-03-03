69b23fb701e197736522ca5b92f5adc2d5d7dc83
privrja <privrja2@fit.cvut.cz>
2019-03-03 13:15:58 +0100

    Dev
    + new SMILES input to JSME editor

--- application/views/blocks/new.php
+++ application/views/blocks/new.php
@@ -9,12 +9,17 @@ use Bbdgnc\Finder\Enum\ServerEnum;
 <script src="<?= AssetHelper::jsJsme() ?>"></script>
 <script>
 
+    document.addEventListener('input', readSmiles);
+
     /**
      * This function will be called after the JavaScriptApplet code has been loaded.
      */
     function jsmeOnLoad() {
         jsmeApplet = new JSApplet.JSME("jsme_container", "500px", "500px");
-        jsmeApplet.readGenericMolecularInput('CCC');
+    }
+
+    function readSmiles() {
+        jsmeApplet.readGenericMolecularInput(document.getElementById('txt-block-smiles').value);
     }
 
     /**
@@ -44,6 +49,9 @@ use Bbdgnc\Finder\Enum\ServerEnum;
 
         <label for="txt-block-mass">Monoisotopic Residue Mass</label>
         <input type="text" id="txt-block-mass" name="<?= Front::BLOCK_MASS ?>"/>
+
+        <label for="txt-block-mass">SMILES</label>
+        <input type="text" id="txt-block-smiles" name="<?= Front::BLOCK_SMILES ?>"/>
 
         <label for="txt-block-losses">Neutral Losses</label>
         <input type="text" id="txt-block-losses" name="<?= Front::BLOCK_NEUTRAL_LOSSES ?>"/>
