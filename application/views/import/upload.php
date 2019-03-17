6c1ab377bee26496c08afce3f5f66375690c22a2
privrja <privrja@gmail.com>
2019-03-17 16:42:00 +0100

    Dev
    | remove info about uploaded file

--- application/views/import/upload.php
+++ application/views/import/upload.php
@@ -6,11 +6,6 @@
     <article>
         <h2>Import</h2>
         <h3>Your file was successfully uploaded!</h3>
-        <ul>
-            <?php foreach ($upload_data as $item => $value): ?>
-                <li><?php echo $item; ?>: <?php echo $value; ?></li>
-            <?php endforeach; ?>
-        </ul>
         <p><?= anchor('import/upload', 'Upload Another File!') ?></p>
     </article>
 </div>
