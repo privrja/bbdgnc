3dbdac54d21a9b6b49174ac5f6c034a62d50f257
privrja <privrja@gmail.com>
2019-04-10 12:44:03 +0200

    Dev
    | fix footer
    | fix Smiles Drawer version

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,11 +4,11 @@
     <nav>
         <ul class="footer">
             <li class="footer">
-                <a class="a-black" href="https://github.com/reymond-group/smilesDrawer">SMILEs Drawer</a>
+                <a class="a-black" href="https://github.com/reymond-group/smilesDrawer">Smiles Drawer</a>
             </li>
-            <li class="footer">
-                <a class="a-black" href="http://dx.doi.org/10.1021/acs.jcim.7b00425">JCIM</a>
-            </li>
+<!--            <li class="footer">-->
+<!--                <a class="a-black" href="http://dx.doi.org/10.1021/acs.jcim.7b00425">JCIM</a>-->
+<!--            </li>-->
             <li class="footer">
                 <a class="a-black" href="https://jcheminf.biomedcentral.com/articles/10.1186/1758-2946-5-24">JSME</a>
             </li>
@@ -17,6 +17,12 @@
             </li>
             <li class="footer">
                 <a class="a-black" href="https://pubchem.ncbi.nlm.nih.gov">PubChem</a>
+            </li>
+            <li class="footer">
+                <a class="a-black" href="https://www.ebi.ac.uk/chebi/">ChEBI</a>
+            </li>
+            <li class="footer">
+                <a class="a-black" href="https://ms.biomed.cas.cz/cyclobranch/docs/html/">CycloBranch</a>
             </li>
 <!--            <li class="footer">-->
 <!--                <a class="a-black" href="http://bioinfo.lifl.fr/norine" title="Norine, the knowledgebase dedicated to non-ribosomal peptides, is now open to crowdsourcing">Norine</a>-->
